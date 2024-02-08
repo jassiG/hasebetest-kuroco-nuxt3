8858fa0504156b76f5d6bf9831edb84463416e84
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2024-02-08 19:02:54 +0900

    add pagenation to new list

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -8,13 +8,24 @@
     </div>
     <p>Test With Comment</p>
     <nuxt-link to="/news/test_with_comment">Test With Comment</nuxt-link>
+    <Pagenator v-bind="{ ...response.pageInfo }" @pageUpdate="fetchData"/>
   </div>
 </template>
 
 <script setup>
 const config = useRuntimeConfig();
-const { data: response } = await useFetch('/rcms-api/1/news',{
-  baseURL:config.public.apiBase,
-  credentials: 'include',
-});
+const route = useRoute();
+const page = ref(route.query.page || 1);
+const response = ref(null);
+const loading = ref(false);
+
+await fetchData(page.value);
+
+async function fetchData(page) {
+  const res = await $fetch(`${config.public.apiBase}/rcms-api/1/news?pageID=${page}`, {
+    method: 'GET',
+    credentials: 'include',
+  });
+  response.value = res;
+}
 </script>
