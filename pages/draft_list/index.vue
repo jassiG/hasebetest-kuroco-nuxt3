e0236f9aac004ce1eaec2f85a1b68768430780c2
HisashiHasebe <hhasebe@diverta.co.jp>
2024-12-20 10:28:36 +0800

    Add draft list

---
+++ pages/draft_list/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>draft list</p>
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+const { data: response } = await useFetch("https://sample-support-kuroco.a.kuroco.app/rcms-api/35/topics/draft_list", {
+  //baseURL: config.public.apiBase,
+  credentials: "include",
+  headers: {
+    accept: "application/json",
+  },
+});
+</script>
